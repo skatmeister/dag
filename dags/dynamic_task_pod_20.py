83ae1e4242c30405e2fe00040f4d27ea74f1dc23
Chris Bailey <chris.bailey@sas.com>
2023-02-17 09:08:40 -0500

    rename graph

--- dags/dynamic_task_pod_20.py
+++ dags/dynamic_task_pod_20.py
@@ -18,7 +18,6 @@ with DAG(dag_id="dynamic_task_pod_n", start_date=datetime.utcnow()) as dag:
         print(f"Total was {total}")
         return total
 
-    # mylist=createList(43)
     mylist = [i+1 for i in range(43)]
     added_values = add_one.expand(x=mylist)
     sum_it(added_values)
